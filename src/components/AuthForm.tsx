aa76c93a72a4746c43a5d88808065a8b54d26eed
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-08-05 16:12:38 +0330

    AuthForm.tsx component

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,32 @@
+import { authFormData, authSchema } from "@/lib/validation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+
+export default function AuthForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<authFormData>({ resolver: yupResolver(authSchema) });
+
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const resp = await fetch("https://randomuser.me/api/?results=1&nat=us");
+    const data = await resp.json();
+    const user = data.results[0];
+
+    localStorage.setItem("user", JSON.stringify(user));
+    router.push("/dashboard");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input lable="شماره موبایل" {...register("phone")} />
+      <Button type="submit">ورود</Button>
+    </form>
+  );
+}
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,8 +2,8 @@ interface buttonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button = ({ children, ...props }: buttonProps) => {
-  <button {...props}>{children}</button>;
-};
+const Button = ({ children, ...props }: buttonProps) => (
+  <button {...props}>{children}</button>
+);
 
 export default Button;
